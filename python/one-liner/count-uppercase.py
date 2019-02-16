733b2fbac9270e50c44e8e3bfa2d3d8a45317956
Ham Nguyen <47585916+Hamng@users.noreply.github.com>
2019-02-15 19:28:50 -0800

    Update count-uppercase.py

--- python/one-liner/count-uppercase.py
+++ python/one-liner/count-uppercase.py
@@ -7,4 +7,10 @@ python -c "import sys; print(sum(1 for lines in open(sys.argv[1]).readlines() fo
 python -c "import sys; import re; print(sum(len(re.split('[A-Z]',lines))-1 for lines in open(sys.argv[1]).readlines()))" filename
 
 # Even fancier, doing so for many files
+# why the -1?  The number of elements in the post-split list is always 1 more than num of matches
+# E.g. no matches => not splitted => len=1, count=0
+# single matched: lines='   A  wha ' => [before_match, after_match] => count=len-1=1
+# This works even if matched at begin of string (e.g. lines='A   ' => before_match='')
+# or matched at end (lines='   A' => after_match='')
+# or both (lines='A' => before_ and after_ are both '')
 python -c "import fileinput; import re; print(sum(len(re.split('[A-Z]',lines))-1 for lines in fileinput.input()))" file(s)...
