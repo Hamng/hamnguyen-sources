e9cf8a8b68841b6adb61daaad393f99fa907cb76
Ham Nguyen <dont@mailinator.com>
2019-02-22 19:19:02 -0800

    1

--- python/delete_backspace.py
+++ python/delete_backspace.py
@@ -6,14 +6,13 @@ Created on Fri Feb 22 06:44:22 2019
 
 (Java) Interview question from Samsung (from HackerRank)
 
-Edit an array of keypress chars to process r"\b" by removing it
-and the preceding char.
+Given an array of chars (of keys pressed), remove r"\b" and the preceding char.
 Notes:
     1.  This is NOT the single backspace char ('\b'), but 2 consecutive
         chars: a backslash char ('\') followed by a 'b'
-    2.  Must handle multiple occurences of "\b" (of course).
+    2.  Must handle multiple occurrences of r"\b", might be adjacent.
     3.  Must handle (possibly multiple) r"\b" at the beginning of the array.
-    4.  Must handle "excessive \b"; i.e. more r"\b" than avail chars.
+    4.  Must handle _excessive_ r"\b"; i.e. more r"\b" than avail chars.
     5.  No special treatment for other backslash-combo;
         i.e. treat them normally as 2 consecutive chars.
         e.g. r"\1\b2" => r"\2" (deleting "1")
@@ -37,28 +36,29 @@ def delete_backspace(s):
                                  \\     # a literal backslash
                                  b      # then 'b'
                                 )+      # 1-or-more occurences
-                                (.*)    # group \2: rest
                             """, re.VERBOSE)    # to document pattern
     #print("leading_bs<%s>" % leading_backspaces)
-    s = re.sub(leading_bs, r"\2", s)
+    s = re.sub(leading_bs, "", s)
     #print("-lead_bs<%s>" % s)
 
     non_lead_bs = re.compile(r"""
                                  .      # any single char
                                  \\     # a literal backslash
                                  b      # then 'b'
-                                 (.*)   # group \1: rest
                              """, re.VERBOSE)   # to document pattern
-    s, nsubs = re.subn(non_lead_bs, r"\1", s, count=1)  # do only 1 sub
+    s, nsubs = re.subn(non_lead_bs, "", s, count=1)  # do only 1 sub
     #print("-char+bs<%s>" % s)
     while nsubs > 0:
-        s = re.sub(leading_bs, r"\2", s)
+        s = re.sub(leading_bs, "", s)
         #print("-lead_bs<%s>" % s)
-        s, nsubs = re.subn(non_lead_bs, r"\1", s, count=1)  # do only 1 sub
+        s, nsubs = re.subn(non_lead_bs, "", s, count=1)  # do only 1 sub
         #print("-char+bs<%s>" % s)
     print("%s" % s)
 
 if __name__ == '__main__':
     #delete_backspace(input())
+    delete_backspace(r"\1\b2: '1' removed");
+    delete_backspace(r"\\bA: first '\' removed");
+    delete_backspace(r"123\b\bABC: '23' removed bc consecutive bs");
     delete_backspace(r"\b\b\b  with lead\bDing bs A\bB and em\bMbedde\bEd some\b\b\bOMEwhere")
     delete_backspace(r"\b\b\b\b123456\b\b\b\b\b\b\b\b\b\bExtra bs removed")
