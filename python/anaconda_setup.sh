159e260b14f555abb2b84b3c9df10476a56b7977
Ham Nguyen <ham.nguyen@partner.samsung.com>
2020-06-02 11:49:51 -0700

    Fixed such that the actual anaconda[23]/ subdir can now reside in any path containing the anaconda[23]_setup.sh symlink; i.e. anaconda[23]/ no longer needs to reside in ${HOME}

--- python/anaconda_setup.sh
+++ python/anaconda_setup.sh
@@ -1,22 +1,23 @@
-# Use: source anaconda[23]_setup.sh
+# Use: source path/to/anaconda[23]_setup.sh
 # (with anaconda[23]_setup.sh being symlinks to anaconda_setup.sh
+# And path/to/ should also contain the actual anaconda[23]/
 
-__anaconda=$(basename ${BASH_SOURCE} _setup.sh)
+__anaconda=$(readlink -f $(dirname ${BASH_SOURCE}))/$(basename ${BASH_SOURCE} _setup.sh)
 #echo "BASH_SOURCE=<${BASH_SOURCE}>" "__anaconda=${__anaconda}"
 #exit 0
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('${HOME}/${__anaconda}/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$('${__anaconda}/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "${HOME}/${__anaconda}/etc/profile.d/conda.sh" ]; then
-        . "${HOME}/${__anaconda}/etc/profile.d/conda.sh"
+    if [ -f "${__anaconda}/etc/profile.d/conda.sh" ]; then
+        . "${__anaconda}/etc/profile.d/conda.sh"
 	# Above prepends ${__anaconda}/condabin to $PATH
 	#$SHELL -cx 'echo prepend condabin ${PATH}'
     else
-        export PATH="${HOME}/${__anaconda}/bin:$PATH"
+        export PATH="${__anaconda}/bin:$PATH"
 	# Above prepends ${__anaconda}/bin to $PATH
 	$SHELL -cx 'echo prepend bin ${PATH}'
     fi
