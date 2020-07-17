951d85e62ecb06b2040a769460075d140cc14358
Ham Nguyen <47585916+Hamng@users.noreply.github.com>
2020-07-17 09:58:14 -0700

    Add files via upload

--- Scripts/loop4ever.sh
+++ Scripts/loop4ever.sh
@@ -1,1 +1,20 @@
-ffff
+#/bin/bash
+
+# loop4ever.sh secs_to_sleep command ...
+
+secs_to_sleep=$1
+shift
+
+c=0
+while true;
+do
+    ((c++))
+    #echo -e "\n$(date +'%F %T:') ===== Iteration #$c =====\n$@"
+    # Fancier way to show heading using Bash builtin printf
+    # '%(datefmt)T' -1: display current time in datefmt format
+    # "$*": quote all args as a single string to show
+    printf "\n%(%F %T)T: ===== Iteration #%u =====\n%s\n" -1 $c "$*"
+    "$@"
+    sleep $secs_to_sleep
+    echo -e "\n\n"
+done
